c5f647aa2739976d623d15e5dcd75efb55b0d5f6
Alex Shumilov <alex.shumilov@rationalexponent.com>
2025-06-20 13:17:41 +0400

    Add new quality testing and cost optimization modules to __all__ exports

--- src/jirascope/pipeline/__init__.py
+++ src/jirascope/pipeline/__init__.py
@@ -3,5 +3,42 @@
 from .embedding_processor import EmbeddingProcessor, AdaptiveBatcher
 from .quality_validator import EmbeddingQualityValidator
 from .incremental_processor import IncrementalProcessor
+from .rag_quality_tester import (
+    RAGQualityTester, 
+    TestQuery, 
+    RAGTestResult,
+    RAGQualityReport,
+    EmbeddingConsistencyReport, 
+    BatchSizeBenchmark,
+    PerformanceBenchmark
+)
+from .test_query_framework import (
+    TestQueryManager,
+    TestQueryCollection,
+    TestCategory
+)
+from .comprehensive_quality_tester import (
+    ComprehensiveQualityTester,
+    QualityTestPlan,
+    FullQualityReport
+)
 
-__all__ = ["EmbeddingProcessor", "AdaptiveBatcher", "EmbeddingQualityValidator", "IncrementalProcessor"]
+__all__ = [
+    "EmbeddingProcessor", 
+    "AdaptiveBatcher", 
+    "EmbeddingQualityValidator", 
+    "IncrementalProcessor",
+    "RAGQualityTester",
+    "TestQuery",
+    "RAGTestResult",
+    "RAGQualityReport",
+    "EmbeddingConsistencyReport",
+    "BatchSizeBenchmark",
+    "PerformanceBenchmark",
+    "TestQueryManager",
+    "TestQueryCollection",
+    "TestCategory",
+    "ComprehensiveQualityTester",
+    "QualityTestPlan",
+    "FullQualityReport"
+]
--- src/jirascope/utils/__init__.py
+++ src/jirascope/utils/__init__.py
@@ -1,5 +1,45 @@
 """Utility modules for JiraScope."""
 
 from .logging import setup_logging, CostTracker, StructuredLogger
+from .cost_optimizer import (
+    CostOptimizer,
+    UsagePatternAnalyzer,
+    APICall,
+    PromptEfficiencyAnalysis,
+    BatchOptimizationSuggestions,
+    OptimizationSuggestion,
+    ClaudeUsageAnalysis,
+    CostPredictions,
+    CostTrends,
+    ComprehensiveCostReport,
+    BudgetAlert
+)
+from .cost_reporter import (
+    AdvancedCostReporter,
+    AdvancedCostReport,
+    CostEfficiencyReport,
+    ApiUsageBreakdown,
+    BudgetConfig
+)
 
-__all__ = ["setup_logging", "CostTracker", "StructuredLogger"]
+__all__ = [
+    "setup_logging", 
+    "CostTracker", 
+    "StructuredLogger",
+    "CostOptimizer",
+    "UsagePatternAnalyzer",
+    "APICall",
+    "PromptEfficiencyAnalysis",
+    "BatchOptimizationSuggestions",
+    "OptimizationSuggestion",
+    "ClaudeUsageAnalysis",
+    "CostPredictions",
+    "CostTrends",
+    "ComprehensiveCostReport",
+    "BudgetAlert",
+    "AdvancedCostReporter",
+    "AdvancedCostReport",
+    "CostEfficiencyReport",
+    "ApiUsageBreakdown",
+    "BudgetConfig"
+]
